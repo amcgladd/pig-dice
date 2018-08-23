bf16ae7f9dfe47a799dcddaedc5d8468c8c7e231
A.J. McGladdery and Krystal Foster <a.j.@>
2018-08-23 11:10:02 -0700

    add modal gif

--- js/scripts.js
+++ js/scripts.js
@@ -68,7 +68,7 @@ Player.prototype.hold = function() {
   this.roll = 0;
   this.turnTotal = 0;
   switchPlayer();
-  if(this.playerTotal >= 20) {
+  if(this.playerTotal >= 2) {
     $(".winner-modal").text(this.name);
     $("#myModal").modal('show');
     }
